0a84f054b3e5a85add9ad42c6602b6136b598610
David Tarkhnishvili <datotarkh@gmail.com>
2024-11-15 10:28:23 +0100

    homework

--- bookshop/views.py
+++ bookshop/views.py
@@ -6,18 +6,39 @@ from .forms import BookForm
 
 
 def index(request):
-    books = Book.objects.all()
-    return render(request, 'index.html', {'books': books})
+    book_name = request.GET.get('book_name')
+    category = request.GET.getlist('category')
+
+    if book_name:
+        books = Book.objects.filter(title__icontains=book_name)
+    elif category:
+        books = Book.objects.filter(category__in=category)
+    else:
+        books = Book.objects.all()
 
 
 
+
+    categories = Category.objects.all()
+    book_by_genres = {}
+
+
+    for category in categories:
+        genre = category.genre
+        if genre not in book_by_genres:
+            book_by_genres[genre] = []
+        book_by_genres[genre].append(category)
+
+    print(book_by_genres)
+
+    return render(request, 'index.html', {'books': books, 'book_by_genres': book_by_genres})
 
 def add_book(request):
     if request.method == 'POST':
         form = BookForm(request.POST, request.FILES)
         if form.is_valid():
             book = form.save()
-        return redirect('/bookshop')
+        return redirect('bookshop:index')
 
     else:
         form = BookForm()
--- store/urls.py
+++ store/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bookshop/', include('bookshop.urls')),
+    path('bookshop/', include('bookshop.urls', namespace='bookshop')),
 ]
